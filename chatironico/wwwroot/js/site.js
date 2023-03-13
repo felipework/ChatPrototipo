d4d2518da157fc5bd41b33ad25b24038599070ea
unknown <emersonbljr2802@gmail.com>
2023-03-13 13:18:18 -0300

    adicionando chat

---
+++ chatironico/wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿const inputQuestion = document.getElementById("inputQuestion");
+const result = document.getElementById("result");
+
+inputQuestion.addEventListener("keypress", (e) => {
+  if (inputQuestion.value && e.key === "Enter") SendQuestion();
+});
+
+const OPENAI_API_KEY = "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";
+
+function SendQuestion() {
+  var sQuestion = inputQuestion.value;
+
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: sQuestion,
+      temperature: 0.5, // criatividade na resposta
+      max_tokens: 600, // tamanho da resposta
+      top_p: 0.3,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.0,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (result.value) result.value += "\n";
+
+      if (json.error?.message) {
+        result.value += `Error: ${json.error.message}`;
+      } else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Sem resposta";
+
+        result.value += "Ana: " + text;
+      }
+
+      result.scrollTop = result.scrollHeight;
+    })
+    .catch((error) => console.error("Error:", error))
+    .finally(() => {
+      inputQuestion.value = "";
+      inputQuestion.disabled = false;
+      inputQuestion.focus();
+    });
+
+  if (result.value) result.value += "\n\n\n";
+
+  result.value += `Eu: ${sQuestion}`;
+  inputQuestion.value = "Carregando...";
+  inputQuestion.disabled = true;
+
+  result.scrollTop = result.scrollHeight;
+}
